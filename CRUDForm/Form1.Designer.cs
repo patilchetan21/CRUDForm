e5d1ac29c5481d6aacc506c79b2567c8bfe0491d
Chetan Patil <patilchetan2110@gmail.com>
2023-03-28 12:19:48 +0530

    Add project files.

---
+++ CRUDForm/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CRUDForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.prod_type = new System.Windows.Forms.TextBox();
+            this.price = new System.Windows.Forms.TextBox();
+            this.prod_id = new System.Windows.Forms.TextBox();
+            this.prod_name = new System.Windows.Forms.TextBox();
+            this.prod_desc = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.displayTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.displayTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(321, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "E-Shop";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(150, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(150, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Product Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(150, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Product ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(150, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(150, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(188, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Product Description";
+            // 
+            // prod_type
+            // 
+            this.prod_type.Location = new System.Drawing.Point(387, 73);
+            this.prod_type.Name = "prod_type";
+            this.prod_type.Size = new System.Drawing.Size(188, 20);
+            this.prod_type.TabIndex = 6;
+            this.prod_type.TextChanged += new System.EventHandler(this.prod_type_TextChanged);
+            // 
+            // price
+            // 
+            this.price.Location = new System.Drawing.Point(387, 179);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(188, 20);
+            this.price.TabIndex = 7;
+            this.price.TextChanged += new System.EventHandler(this.price_TextChanged);
+            // 
+            // prod_id
+            // 
+            this.prod_id.Location = new System.Drawing.Point(387, 144);
+            this.prod_id.Name = "prod_id";
+            this.prod_id.Size = new System.Drawing.Size(188, 20);
+            this.prod_id.TabIndex = 8;
+            this.prod_id.TextChanged += new System.EventHandler(this.prod_id_TextChanged);
+            // 
+            // prod_name
+            // 
+            this.prod_name.Location = new System.Drawing.Point(387, 111);
+            this.prod_name.Name = "prod_name";
+            this.prod_name.Size = new System.Drawing.Size(188, 20);
+            this.prod_name.TabIndex = 9;
+            this.prod_name.TextChanged += new System.EventHandler(this.prod_name_TextChanged);
+            // 
+            // prod_desc
+            // 
+            this.prod_desc.Location = new System.Drawing.Point(387, 210);
+            this.prod_desc.Name = "prod_desc";
+            this.prod_desc.Size = new System.Drawing.Size(188, 20);
+            this.prod_desc.TabIndex = 10;
+            this.prod_desc.TextChanged += new System.EventHandler(this.prod_desc_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(128, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 38);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(491, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 38);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(370, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 38);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(249, 247);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 38);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Display";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // displayTable
+            // 
+            this.displayTable.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.displayTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.displayTable.Location = new System.Drawing.Point(154, 291);
+            this.displayTable.Name = "displayTable";
+            this.displayTable.Size = new System.Drawing.Size(421, 302);
+            this.displayTable.TabIndex = 15;
+            this.displayTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(800, 605);
+            this.Controls.Add(this.displayTable);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.prod_desc);
+            this.Controls.Add(this.prod_name);
+            this.Controls.Add(this.prod_id);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.prod_type);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.displayTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox prod_type;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.TextBox prod_id;
+        private System.Windows.Forms.TextBox prod_name;
+        private System.Windows.Forms.TextBox prod_desc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView displayTable;
+    }
+}
+
---
+++ CRUDForm/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-NR2M9QM\\SQLEXPRESS;Initial Catalog=CRUDForm;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ut values(@prod_type,@prod_name,@prod_id,@price,@prod_desc)",con);
+            cmd.Parameters.AddWithValue("@prod_type", prod_type.Text);
+            cmd.Parameters.AddWithValue("@prod_name", prod_name.Text);
+            cmd.Parameters.AddWithValue("@prod_id", int.Parse(prod_id.Text));
+            cmd.Parameters.AddWithValue("@price", double.Parse(price.Text));
+            cmd.Parameters.AddWithValue("@prod_desc", prod_desc.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully Inserted to the database...");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-NR2M9QM\\SQLEXPRESS;Initial Catalog=CRUDForm;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update ut set prod_type=@prod_type, prod_name=@prod_name,price=@price,prod_desc=@prod_desc where prod_id=@prod_id", con);
+            cmd.Parameters.AddWithValue("@prod_type", prod_type.Text);
+            cmd.Parameters.AddWithValue("@prod_name", prod_name.Text);
+            cmd.Parameters.AddWithValue("@prod_id", int.Parse(prod_id.Text));
+            cmd.Parameters.AddWithValue("@price", double.Parse(price.Text));
+            cmd.Parameters.AddWithValue("@prod_desc", prod_desc.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Updated to the database...");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-NR2M9QM\\SQLEXPRESS;Initial Catalog=CRUDForm;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete ut where prod_id=@prod_id",con);
+            cmd.Parameters.AddWithValue("@prod_id", int.Parse(prod_id.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Deleted to the database...");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-NR2M9QM\\SQLEXPRESS;Initial Catalog=CRUDForm;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from ut", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            displayTable.DataSource = dt;
+            con.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void prod_type_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prod_name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prod_id_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void price_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prod_desc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
